2475db883f2413ec2027addca670200ce2ff1148
trmttm <96355189+trmttm@users.noreply.github.com>
2021-12-27 20:28:33 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='interface',
+    version='1.0.0',
+    packages=['interface_view','interface_spreadsheet', 'interface_fm','interface_fm_calculator','interface_keymaps','interface_udf_builder', 'interface_gateway_fm','interface_mouse'],
+    url='',
+    license='',
+    author='Taro Yamaka',
+    author_email='t.yamaka.1213@gmail.com',
+    description=''
+)
