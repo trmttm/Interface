51b9f38540bd5ce18127d183ad2020b83f52df6b
trmttm <96355189+trmttm@users.noreply.github.com>
2022-01-16 07:04:12 +0400

    Implemented set_fill_color

--- interface_view/__init__.py
+++ interface_view/__init__.py
@@ -52,6 +52,10 @@ class ViewABC(ABC):
         pass
 
     @abstractmethod
+    def set_fill_color(self, view_model: dict):
+        pass
+
+    @abstractmethod
     def add_line(self, view_model: dict):
         pass
 
