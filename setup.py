74edd428e1fbfa007a4425549c206c7d4401c60e
trmttm <96355189+trmttm@users.noreply.github.com>
2022-10-12 06:14:07 +0400

    Isolate tk_interface ...2

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 setup(
     name='interface',
     version='1.0.0',
-    packages=['interface_view','interface_spreadsheet', 'interface_fm','interface_fm_calculator','interface_keymaps','interface_udf_builder', 'interface_gateway_fm','interface_mouse'],
+    packages=['interface_view', 'interface_spreadsheet', 'interface_fm', 'interface_fm_calculator', 'interface_keymaps',
+              'interface_udf_builder', 'interface_gateway_fm', 'interface_mouse', 'tk_interface'],
     url='',
     license='',
     author='Taro Yamaka',
